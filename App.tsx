18d583f4a3071d88a2185eab893a9af516b3a4db
Rafael Barreto <rafaelhbarreto@gmail.com>
2024-11-27 14:48:51 -0300

    feat: add fonts from expo

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { Group } from "@screens/Groups"
 import { ThemeProvider } from "styled-components";
+import { 
+  useFonts,  
+  Roboto_400Regular,
+  Roboto_700Bold
+} from "@expo-google-fonts/roboto";
 
 import theme from "./src/theme";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Group />
+      { fontsLoaded ? <Group /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
