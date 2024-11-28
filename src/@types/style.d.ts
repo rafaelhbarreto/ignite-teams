9bcd3a575d4ec4251b24dccaa02f33cf7d28fa4a
Rafael Barreto <rafaelhbarreto@gmail.com>
2024-11-28 10:47:50 -0300

    fix: fix theme import

--- src/@types/style.d.ts
+++ src/@types/style.d.ts
@@ -1,7 +1,8 @@
 import 'styled-components';
-import theme from 'src/theme';
+import theme from '../theme';
 
 declare module 'styled-components' {
   type ThemeType = typeof theme;
+  
   export interface DefaultTheme extends ThemeType {}
 }
