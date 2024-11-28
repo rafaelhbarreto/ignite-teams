a239f5c8cfd261de849a48e0f136de4bd6d718d1
Rafael Barreto <rafaelhbarreto@gmail.com>
2024-11-28 18:06:32 -0300

    refactor: adjust theme

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,8 +1,8 @@
 import 'styled-components';
 import theme from '../theme';
 
+type ThemeInterface = typeof theme
+
 declare module 'styled-components' {
-  type ThemeType = typeof theme;
-  
-  export interface DefaultTheme extends ThemeType {}
+  export interface DefaultTheme extends ThemeInterface {}
 }
