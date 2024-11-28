a8f8986074190babd2bdbb7a148683ae2b91ee47
Rafael Barreto <rafaelhbarreto@gmail.com>
2024-11-28 11:15:41 -0300

    refactor: adjust container from group screen

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,9 +2,9 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
   background-color: ${({theme}) => theme.COLORS.GREEN_700};
+  padding: 24px;
+  padding-top: 50px
 `
 
 export const Title = styled.Text`
