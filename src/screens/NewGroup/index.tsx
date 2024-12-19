a128fcc9873810350c0d7287e680d94749f504a7
Rafael Barreto <rafaelhbarreto@gmail.com>
2024-12-19 19:09:09 -0300

    feat: add input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Highlight } from "@components/Highlight";
 import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -15,6 +16,7 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="Crie uma nova turma para adicionar as pessoas"
         />
+        <Input />
         <Button 
           text="Criar"
         />
