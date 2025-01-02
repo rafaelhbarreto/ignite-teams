62b3cd01783f1aa610b123eef2fba3e23afa0fa0
Rafael Barreto <rafaelhbarreto@gmail.com>
2025-01-02 09:19:30 -0300

    feat: create specific error

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -2,10 +2,18 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUP_COLLECTION } from "@storage/config";
 
 import { groupGetAll } from "./groupGetAll";
+import { AppError } from "@utils/AppError";
 
 export async function groupCreate(group: string) {
   try {
     const groups = await groupGetAll();
+
+    const grousExists = groups.includes(group);
+    
+    if (grousExists) {
+      throw new AppError('Já existe um grupo cadastrado com este nome');
+    }
+
     await AsyncStorage.setItem(GROUP_COLLECTION, JSON.stringify([...groups, group]));
   } 
   catch (error) {
